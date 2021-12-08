7629ca126accc8db02bf0b219568a26f3c4a2535
Amirossein Dehghan <73764514+amhdhn@users.noreply.github.com>
2021-12-08 18:09:34 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const timerText = document.querySelector('.text');
-const text = ['Hi. I\'m Amirhossein Dehghan.  ', 'I\'m Frontend developer.   ', 'Wellcome to my Github page.   ', 'Scroll down page to see my projects.   '];
+const text = ['Wellcome to my Github page.   ', 'Hi. I\'m Amirhossein Dehghan.  ', 'I\'m Frontend developer.   ', 'Scroll down page to see my projects.   '];
 let textCounter = 0;
 let arrayCounter = 0;
 let endCheck = false;
@@ -76,4 +76,42 @@ for (let index = 0; index < 1; index++) {
     viewPage.innerText = 'View Project';
     webPageListItem.appendChild(viewPage);
     webPageList.appendChild(webPageListItem);
+}
+
+const appList = [
+    ['watch/', 'flipTableClock.jpg', 'Flip Table Clock', 'HtmlpopCsspopJavaScript']
+];
+
+const webAppList = document.querySelector('.webAppList');
+for (let index = 0; index < 1; index++) {
+    const webAppListItem = document.createElement('div');
+    webAppListItem.classList.add('webAppListItem');
+    const appShow = document.createElement('img');
+    appShow.classList.add('appShow');
+    appShow.src = 'Assets/Template/' + appList[index][1];
+    webAppListItem.appendChild(appShow);
+
+    const pageName = document.createElement('p');
+    pageName.classList.add('pageName');
+    pageName.innerText = appList[index][2];
+    webAppListItem.appendChild(pageName);
+
+    const languageUseList = document.createElement('ul');
+    languageUseList.classList.add('languageUseList');
+    const langList = appList[index][3].split('pop');
+    for (const iterator of langList) {
+        const li = document.createElement('li');
+        li.classList.add(iterator);
+        li.innerText = iterator;
+        languageUseList.appendChild(li);
+    }
+    webAppListItem.appendChild(languageUseList);
+
+    const viewPage = document.createElement('a');
+    viewPage.classList.add('viewPage');
+    viewPage.href = appList[index][0];
+    viewPage.target = '_blank';
+    viewPage.innerText = 'View Project';
+    webAppListItem.appendChild(viewPage);
+    webAppList.appendChild(webAppListItem);
 }
